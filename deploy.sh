4c5ae4f04ebb1e69e6459e86fe40561e84ab5c3f
David Peter <mail@david-peter.de>
2023-02-06 14:06:43 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ mv css/main.min.css css/main.css
 closure-compiler js/zepto.min.js js/hyphenate.js js/main.js > js/main.min.js
 
 rsync --archive --force --compress --progress --rsh="ssh" \
-    index.html css js files img shark@kalyke.de:/var/www/html/david-peter.de/
+    index.html sitemap.xml css js files img shark@shark.fish:/var/www/html/david-peter.de/
