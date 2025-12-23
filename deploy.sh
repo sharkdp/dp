e0d616e27737e56e1035a47c98e6562983f11bc2
David Peter <mail@david-peter.de>
2025-12-23 14:27:29 +0100

    Modern CSS instead of Less

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,5 @@
 
 set -euo pipefail
 
-lessc --verbose css/main.less css/main.css
-
-python -m csscompressor css/main.css > css/main.min.css
-mv css/main.min.css css/main.css
-
 rsync --archive --force --compress --progress --rsh="ssh" \
     index.html sitemap.xml css js files img shark@shark.fish:/var/www/html/david-peter.de/
